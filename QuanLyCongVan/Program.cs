cdfed8593de725d34b702ac56c8e04158ef7305d
TranPhai <tranphai792@gmail.com>
2021-12-28 17:45:47 +0700

    push database

--- QuanLyCongVan/Program.cs
+++ QuanLyCongVan/Program.cs
@@ -16,7 +16,7 @@ namespace QuanLyCongVan
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new fxemcongvan());
+            Application.Run(new fdangnhap());
         }
     }
 }
